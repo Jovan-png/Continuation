eaf959803f3ef8606947b422ec2b5a22f159cbdb
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2022-04-25 15:35:07 -0500

    declared arrays

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -6,6 +6,8 @@ namespace CatWorx.BadgeMaker
     {
         static void Main()
         {
+            //Mathmatic Operators
+
             // double side = 3.14;
             // double area = side * side;
             // Console.WriteLine("area: {0}", area);
@@ -25,14 +27,57 @@ namespace CatWorx.BadgeMaker
             // num++;
             // Console.WriteLine(num);
 
+            //Booleans
+
             // bool isCold = true;
             // Console.WriteLine(isCold ? "drink" : "add ice");
             // Console.WriteLine(!isCold ? "drink" : "add ice");
+            // Datatype Conversion
 
-            string stringNum = "2";
-            int intNum = Convert.ToInt16(stringNum);
-            Console.WriteLine(intNum);
-            Console.WriteLine(intNum.GetType());
+            // string stringNum = "2";
+            // int intNum = Convert.ToInt16(stringNum);
+            // Console.WriteLine(intNum);
+            // Console.WriteLine(intNum.GetType());
+
+            // Data Structures
+
+
+            // Populate a Dictionary
+
+
+            // Dictionary<string, int> myScoreBoard = new Dictionary<string, int>();
+
+            // myScoreBoard.Add("firstInning", 10);
+            // myScoreBoard.Add("secondInning", 20);
+            // myScoreBoard.Add("thirdInning", 30);
+            // myScoreBoard.Add("fourthInning", 40);
+            // myScoreBoard.Add("fifthInning", 50);
+
+            //             Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+            //     { "firstInning", 10 },
+            //     { "secondInning", 20},
+            //     { "thirdInning", 30},
+            //     { "fourthInning", 40},
+            //     { "fifthInning", 50}
+            // };
+
+            //             Console.WriteLine("----------------");
+            //             Console.WriteLine("  Scoreboard");
+            //             Console.WriteLine("----------------");
+            //             Console.WriteLine("Inning |  Score");
+            //             Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
+            //             Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
+            //             Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
+            //             Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
+            //             Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+
+            // Arrays 
+
+            string[] favFoods = new string[3] { "pizza", "doughnuts", "icecream" };
+            string firstFood = favFoods[0];
+            string secondFood = favFoods[1];
+            string thirdFood = favFoods[2];
+            Console.WriteLine("I Like {0},{1}, and {2}", firstFood, secondFood, thirdFood);
 
 
 
